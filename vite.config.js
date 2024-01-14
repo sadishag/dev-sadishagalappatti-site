634c20be7166c840f5e538bd8cc10d54de100273
Sadisha Galappatti <sadishag@gmail.com>
2024-01-14 18:58:47 -0500

    updating BaseURL to be an ENV var

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const { BASE_URL } = process.env;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/test-site/",
+  base: `/${BASE_URL}/`,
 });
