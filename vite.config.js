16cfe098db3c6a5d08cc46165bbc6911810ac327
Sadisha Galappatti <sadishag@gmail.com>
2024-01-14 21:28:04 -0500

    updating vite.config.js to use env vars with loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-const { VITE_BASE_URL } = import.meta.env;
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  base: `/${VITE_BASE_URL}/`,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [svgr(), react()],
+    base: `/${env.BASE_URL}/`,
+  };
 });
