2ad064755d3df3522436890f910e4b42b5e7f286
Sadisha Galappatti <sadishag@gmail.com>
2024-01-14 21:14:03 -0500

    updating vite env vars

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-const { BASE_URL } = process.env;
+const { VITE_BASE_URL } = import.meta.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: `/${BASE_URL}/`,
+  base: `/${VITE_BASE_URL}/`,
 });
