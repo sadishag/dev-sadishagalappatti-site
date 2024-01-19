8083d7ec2f4b10364db19eccd0255b6f7f3caed6
Sadisha Galappatti <sadishag@gmail.com>
2024-01-19 18:46:46 -0500

    updating resume icon

--- src/components/TitleSection/TitleSection.jsx
+++ src/components/TitleSection/TitleSection.jsx
@@ -17,7 +17,7 @@ const TitleSection = () => {
         <h1 className="h1-title">Sadisha Galappatti</h1>
         <p className="p-title">Full Stack | Cloud | Developer</p>
         <div className="icon-bar">
-          <a href="resume">
+          <a href="https://cv.sadishagalappatti.ca">
             <FontAwesomeIcon icon={faFilePdf} />
           </a>
           <a href="mailto:sadishag@gmail.com">
@@ -29,7 +29,7 @@ const TitleSection = () => {
           <a href="https://www.linkedin.com/in/sadishag/" target="_blank">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a href="https://x.com/sadishagx" target="_blank">
+          <a href="https://x.com/sadishag" target="_blank">
             <FontAwesomeIcon icon={faXTwitter} />
           </a>
         </div>
